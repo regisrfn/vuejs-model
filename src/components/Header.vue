77d7f89133d4d3a4cc3530eed5ec5202b7db3838
Regis Rufino <regis.rfnrodrigues@gmail.com>
2020-10-28 12:38:58 -0300

    Refactoring

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,12 @@
 <template>
-  <div class="header" :class="{scrollClass:isActive}">
+  <div class="header" :class="{ scrollClass: isActive }">
+    <VueLogo class="logo" alt="logo" />
+
     <input type="checkbox" class="header__btn" id="navbar-btn" />
     <label for="navbar-btn" class="navbar-icon">
       <span class="navbar-icon__line"></span>
     </label>
 
-    <VueLogo class="logo" alt="logo" />
     <nav>
       <ul class="header__links">
         <li>
@@ -18,38 +19,48 @@
           <a href="#">About</a>
         </li>
       </ul>
-      <button>Services</button>
+    </nav>
+    <button>Services</button>
+    <nav>
+      <ul class="header__links">
+        <li>
+          <a href="#">Login</a>
+        </li>
+        <li>
+          <a href="#">Register</a>
+        </li>
+      </ul>
     </nav>
   </div>
 </template>
 
 <script>
-import VueLogo from '@/img/logo.svg'
+import VueLogo from "@/img/logo.svg";
 
 export default {
   data() {
     return {
-      isActive:false
-    }
+      isActive: false,
+    };
   },
   components: {
-    VueLogo
+    VueLogo,
   },
-  created () {
-    window.addEventListener('scroll', this.handleScroll)
+  created() {
+    window.addEventListener("scroll", this.handleScroll);
   },
-  destroyed () {
-    window.removeEventListener('scroll', this.handleScroll)
+  destroyed() {
+    window.removeEventListener("scroll", this.handleScroll);
   },
   methods: {
-    handleScroll (event) {
-      const scrollY = window.scrollY
-      if(scrollY > 10) {
-        this.isActive = true
+    handleScroll(event) {
+      const scrollY = window.scrollY;
+      if (scrollY > 10) {
+        this.isActive = true;
       } else {
-         this.isActive = false
+        this.isActive = false;
       }
-    }
-  }
-}
+    },
+  },
+};
 </script>
